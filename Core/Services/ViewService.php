8291a5329af7b77878e048f2a79bcc63926a2a9f
Maliko <jdheldt@mail.com>
2022-06-25 21:58:21 +0200

    Extend the Template-System
    Importing Sub-Templates are now possible.

--- Core/Services/ViewService.php
+++ Core/Services/ViewService.php
@@ -6,18 +6,20 @@ namespace FastPHP\Core\Services;
 class ViewService {
 
     private $_module;
+    private $_templateData;
 
     public function __construct($sModule)
     {
         $this->_module = $sModule;
     }
 
-    public function render($sTemplatePath, $aTemplateData = null, $bRenderDirect = true) {
+    public function render($sTemplatePath, $aTemplateData = null, $bRenderDirect = true) {        
         $sFilePath = dirname($_SERVER['SCRIPT_FILENAME']) . '/Modules/' . $this->_module . '/Views/' . $sTemplatePath . '.php';
 
         $template = file_get_contents($sFilePath);
 
         if(!empty($aTemplateData)) {
+            $this->_templateData = $aTemplateData;
             extract($aTemplateData, EXTR_SKIP);
         }
 
@@ -30,5 +32,9 @@ class ViewService {
 
         return null;
 
+    }
+    
+    public function import($sTemplatePath) {
+        $this->render($sTemplatePath, $this->_templateData);
     }
 }
